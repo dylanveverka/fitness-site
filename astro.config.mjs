c829aaf8a6591ee9c5975e8f827e2c4eefb83816
Dylan Veverka <dylan.veverka27@gmail.com>
2026-04-15 22:12:05 -0400

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,7 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://fitness.dveverka.com',
+  integrations: [sitemap()],
+});
